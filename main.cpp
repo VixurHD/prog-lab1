c593ef23e93118ba0b5de833fcbddd8dcfad1004
Den-Maria <pozittiden@gmail.com>
2026-03-15 14:13:41 +0700

    base menu

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    char choice;
+    char exit;
+    while (exit == 0) {
+        cout << "Выберите фигуру: "  << endl;
+        cout << "1 — Прямоугольник"  << endl;
+        cout << "2 — Треугольник"    << endl;
+        cout << "3 — Трапеция"       << endl;
+        cout << "4 — Круг"           << endl;
+        cout << "0 — Выйти"          << endl;
+        cout << "\033[5A";cout << "\033[17C"; //Смещение коретки
+        cin  >> choice;
+
+        switch(choice) {
+            case 'a':
+            case '1':
+                // TODO: вызов функции Айхаана Прямоугольник
+                break;
+            case 'b':
+            case '2':
+                // TODO: вызов функции Дениса Треугольник
+                break;
+            case 'c':
+            case '3':
+                // TODO: вызов функции Димы Трапеция
+                break;
+            case 'd':
+            case '4':
+                // TODO: вызов функции Тимофея Круг
+                break;
+            case 'q':
+            case '0':
+                exit = 1;
+                cout << "Выход..." << endl;
+                break;
+            default:
+                cout << "Пожалуйста введите число от 1 до 4 (или a,b,c,d) или используйте 0 для выхода" << endl;
+                break;
+        }
+    }
+    return 0;
+}
